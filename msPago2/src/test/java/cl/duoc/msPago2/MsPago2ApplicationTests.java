fe58e00b330525ff822d63fb3a33eb6c8867b203
Gonzalo <gon.bahamondes@duocuc.cl>
2026-05-17 23:48:23 -0400

    3306

--- msPago2/src/test/java/cl/duoc/msPago2/MsPago2ApplicationTests.java
+++ msPago2/src/test/java/cl/duoc/msPago2/MsPago2ApplicationTests.java
@@ -2,7 +2,6 @@ package cl.duoc.msPago2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootTest
 class MsPago2ApplicationTests {
